207af24871cff92cfa0ae8816ce541608b3a9fec
sanahaju <sanahaju123@gmail.com>
2022-08-25 13:43:18 +0530

    Add project files.

---
+++ e-library.DataLayer/Migrations/20220825074508_initial.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace e_library.DataLayer.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "book_Issues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookId = table.Column<int>(nullable: false),
+                    StudentId = table.Column<int>(nullable: false),
+                    Issue_Date = table.Column<DateTime>(nullable: false),
+                    Return_Date = table.Column<DateTime>(nullable: false),
+                    ActualReturn_Date = table.Column<DateTime>(nullable: false),
+                    Fine = table.Column<double>(nullable: false),
+                    Returned = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_book_Issues", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookName = table.Column<string>(nullable: true),
+                    ISBN = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    Publisher = table.Column<string>(nullable: true),
+                    Published_Year = table.Column<int>(nullable: false),
+                    Edition = table.Column<string>(nullable: true),
+                    Streams = table.Column<int>(nullable: true),
+                    Issued = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    streams = table.Column<int>(nullable: true),
+                    Phone = table.Column<long>(nullable: false),
+                    DOB = table.Column<DateTime>(nullable: false),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "book_Issues");
+
+            migrationBuilder.DropTable(
+                name: "books");
+
+            migrationBuilder.DropTable(
+                name: "students");
+        }
+    }
+}
---
+++ e-library.Entities/Response.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace e_library.Entities
+{
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
